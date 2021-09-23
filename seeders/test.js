a93dc3359c8a37ebae6d236299ec54c763aae2a2
rizkyputra <rizkyagrasyahputra19@gmail.com>
2021-09-23 23:49:39 +0700

    update folder controllers, migration, dan seeders [rizky]

---
+++ seeders/test.js
@@ -0,0 +1,4 @@
+// hanya untuk penanda agar folder dapat terupload ke repo, 
+// karena kalo folder kosong itu tidak bisa di push ke repo.
+
+// silahkan dibuat file seeders ini sendiri sesuai table dari masing-masing task
