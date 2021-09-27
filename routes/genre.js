22ccada7a1f9e48b5ab15194fd3f2713911886fd
rizkyputra <rizkyagrasyahputra19@gmail.com>
2021-09-27 16:32:07 +0700

    add middlewares in genres, and tags [rizky]

--- routes/genre.js
+++ routes/genre.js
@@ -3,9 +3,9 @@ const GenreController = require('../controllers/genreControllers');
 const { authAdmin } = require('../middlewares/authorization');
 const auth = require('../middlewares/authorization');
 
-router.post('/create', GenreController.create);
-router.get('/', GenreController.getAll);
-router.put('/update/:id', GenreController.update);
-router.delete('/delete/:id', GenreController.delete);
+router.post('/create', auth, authAdmin,GenreController.create);
+router.get('/', auth, authAdmin, GenreController.getAll);
+router.put('/update/:id', auth, authAdmin, GenreController.update);
+router.delete('/delete/:id', auth, authAdmin, GenreController.delete);
 
 module.exports = router;
